2981ff573f4dbb1f95356104f9946cb0e912edb8
王治国 <2501660724@qq.com>
2026-08-22 20:18:59 +0800

    第一课变量和基础数据类型

---
+++ lesson1.py
@@ -0,0 +1,14 @@
+name = "王治国"
+age = 26
+height =1.75
+is_student = True
+
+print("我的名字是",name)
+print("今年",age, "岁")
+print("身高",height, "米")
+print("是学生吗？",is_student)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
